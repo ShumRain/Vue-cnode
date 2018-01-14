f404572e4ba4a01848729bded88b6fbb2a5d64a2
shumrain <shumrainchen@gmail.com>
2018-01-14 16:20:25 +0800

    add article comment

--- src/pages/article/article.vue
+++ src/pages/article/article.vue
@@ -15,6 +15,24 @@
 		<div class="article-content">
 			<div v-html="article.content"></div>
 		</div>
+		<div class="article-comment">
+			<div class="comment-item" v-for="reply of article.replies" :key="reply.id">
+				<div><img :src="reply.author.avatar_url"></div>
+				<div>
+					<div class="comment-header">
+						<a href="melo.vc">{{ reply.author.loginname }}</a>
+						<span class="point"></span>
+						<span>1天前</span>
+					</div>
+					<div v-html="reply.content" class="comment-body"></div>
+					<div class="comment-footer">
+						<a href="javascript:;"><span>点赞</span></a>
+						<span class="point"></span>
+						<a href="javascript:;"><span>回复</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
 </template>
 
@@ -24,7 +42,14 @@
 		data() {
 			return {
 				article: {
-					content: ''
+					title: '',
+					visit_count: '',
+					tab: '',
+					content: '',
+					author: {
+						loginname: '',
+					},
+					replies: []
 				}
 			}
 		},
@@ -70,6 +95,55 @@
 		.point {
 			background: #838383;
 		}
+	}
+
+	.article-content {
+		border-bottom: 1px solid #d6d6d6;
+		padding-bottom: 20px;
+	}
+
+	.comment-item {
+		padding: 12px;
+		border-radius: 3px;
+		display: flex;
+		p {
+			padding: 0;
+			margin: 0;
+		}
+		img {
+			width: 48px;
+			height: 48px;
+			border-radius: 3px;
+			margin-right: 12px;
+		}
+	}
+
+	.comment-header {
+		a {
+			font-size: 13px;
+			font-weight: 700;
+		}
+		span {
+			color: #656c7a;
+			font-weight: 500;
+			font-size: 12px;
+		}
+	}
+
+	.comment-footer {
+		a {
+			font-size: 13px;
+			color: #656c7a;
+			font-weight: 500;
+			transition: all .3s ease-out;
+			&:hover {
+				color: #2a2e2e;
+			}
+		}
+		.point {
+			background: #656c7a;
+		}
+
 	}
 </style>
 
